2e3666a65e241611631b5da7bb62c40286ccb789
Cloud User <ec2-user@ip-10-0-1-24.ec2.internal>
2024-12-13 02:04:17 +0000

    Added book to title:

--- parse_xml.c
+++ parse_xml.c
@@ -17,7 +17,8 @@ void printBookTitles(const char *filename) {
         if (curNode->type == XML_ELEMENT_NODE && xmlStrcmp(curNode->name, (const xmlChar *)"book") == 0) {
             for (xmlNode *bookChild = curNode->children; bookChild; bookChild = bookChild->next) {
                 if (bookChild->type == XML_ELEMENT_NODE && xmlStrcmp(bookChild->name, (const xmlChar *)"title") == 0) {
-                    printf("Title: %s\n", xmlNodeGetContent(bookChild));
+                    printf("Book Title: %s\n", xmlNodeGetContent(bookChild));
+
                 }
             }
         }
